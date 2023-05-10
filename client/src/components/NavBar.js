34a2f034619dec121224586ddc7b8fd60c8a24ee
john Caropino <jccaropino7786@live.com>
2023-05-10 13:23:14 -0700

    stuck on React router

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function NavBar(){
     return(
             <div id = "links">
             <ul class = "nav">
-                <li><Link to="/">Home</Link></li>
+                {/* <li><Link to="/">Home</Link></li> */}
                 <li>Players</li>
                 <li>My Team</li>
                 <li>Log in/ Log out</li>
